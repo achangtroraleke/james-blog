3d4eaef3d975f6a004a4368ff9db5b7722dd6a54
Atachart Changtroraleke <atachart.changtroraleke@gmail.com>
2022-11-13 20:14:01 -0500

    Add PostgresSQL

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ Bootstrap(app)
 
 ##CONNECT TO DB
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL",
-                                                       "sqlite://blog.db")
+                                                       "sqlite:///blog.db")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 login_manager = LoginManager()
